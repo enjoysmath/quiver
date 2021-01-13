74b75609ce60b1c7d64d181d7525677d44730bd6
FruitfulApproach <fruitfulapproach@gmail.com>
2021-01-12 16:08:55 -0800

    got collectstatic manage.py command to work locally

--- QuiverDatabase/settings.py
+++ QuiverDatabase/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 def neo4j_url():
-    import os
     url = os.environ['NEO4J_SCHEMA'] + "://"
     url += os.environ['NEO4J_USERNAME'] + ":"
     url += os.environ['NEO4J_PASSWORD'] + "@"
